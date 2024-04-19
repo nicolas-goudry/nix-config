4345480eb8ca832a164906bc4b677e9de05f156a
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-20 01:22:25 +0200

    fix(pkgs): missing catppuccin-alacritty hash

--- pkgs/catppuccin-alacritty.nix
+++ pkgs/catppuccin-alacritty.nix
@@ -11,7 +11,7 @@ stdenvNoCC.mkDerivation {
     owner = "catppuccin";
     repo = "alacritty";
     rev = "94800165c13998b600a9da9d29c330de9f28618e";
-    hash = lib.fakeHash;
+    hash = "sha256-Pi1Hicv3wPALGgqurdTzXEzJNx7vVh+8B9tlqhRpR2Y=";
   };
 
   installPhase = ''
