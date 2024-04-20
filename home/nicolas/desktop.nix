8658af6e5312f16731392d3a1cfb83f8a0d302e4
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-21 01:22:01 +0200

    fix(home/nicolas/desktop): do not use alacritty from store

--- home/nicolas/desktop.nix
+++ home/nicolas/desktop.nix
@@ -21,7 +21,7 @@ in
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Control><Alt>t";
-      command = "${pkgs.alacritty}/bin/alacritty";
+      command = "alacritty";
       name = "Terminal";
     };
 
