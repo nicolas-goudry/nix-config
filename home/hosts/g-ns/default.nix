5791f1ee27b22f9766167e380238f9b13b25a863
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-23 01:24:19 +0200

    feat(home/g-ns): force git email

--- home/hosts/g-ns/default.nix
+++ home/hosts/g-ns/default.nix
@@ -1,7 +1,11 @@
-{ hostname, username, ... }:
+{ hostname, lib, pkgs, username, ... }:
 
 {
   home.shellAliases = {
     switch-home = "cd ~/nixstrap && home-manager --extra-experimental-features flakes --extra-experimental-features nix-command switch -b backup --flake .#${username}@${hostname} && cd -";
+  };
+
+  programs.git = {
+    userEmail = lib.mkForce "nicolas.goudry-ext@numspot.com";
   };
 }
