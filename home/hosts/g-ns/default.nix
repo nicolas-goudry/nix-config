a91a1521670fedd68a861bb6e95ca70162a19bc7
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-23 19:02:38 +0200

    refactor(home/g-ns): remove irrelevant alias to switch-home

--- home/hosts/g-ns/default.nix
+++ home/hosts/g-ns/default.nix
@@ -1,14 +1,12 @@
-{ hostname, lib, outputs, pkgs, username, ... }:
+{ lib, outputs, pkgs, ... }:
 
 {
-  home.shellAliases = {
-    switch-home = "cd ~/nixstrap && home-manager --extra-experimental-features flakes --extra-experimental-features nix-command switch -b backup --flake .#${username}@${hostname} && cd -";
-  };
-
-  programs = {
+  home.programs = {
+    # Use Alacritty wrapped by NixGL
     alacritty.package = lib.mkForce (outputs.libx.wrapNixGL { pkg = pkgs.unstable.alacritty; });
 
     git = {
+      # Override default git email
       userEmail = lib.mkForce "nicolas.goudry-ext@numspot.com";
     };
   };
