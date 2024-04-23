a9f53c8efaab392e762965e9426e6ee3224d4048
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-23 16:18:09 +0200

    refactor(home/nicolas): install some packages only on some hosts

--- home/users/nicolas/default.nix
+++ home/users/nicolas/default.nix
@@ -1,21 +1,21 @@
-{ pkgs, ... }:
+{ hostname, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     bruno
-    dbeaver
-    discord
     gitkraken
-    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
     kubectl
     kubernetes-helm
     k9s
+  ] ++ lib.optionals (hostname == "g-aero") [
+    dbeaver
+    discord
+    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
     obsidian
     pika-backup
     qbittorrent
     slack
     spotify
-    teleport
     uget
   ];
 
