3f2999126a96e44efb32df19e8ac5f14e5cabcec
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-23 15:53:08 +0200

    feat(lib): add helper function to wrap package with nixgl

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,15 @@
 { inputs, outputs, stateVersion, ... }:
 
 {
+  # Supported systems
+  forAllSystems = inputs.nixpkgs.lib.genAttrs [
+    "aarch64-linux"
+    "i686-linux"
+    "x86_64-linux"
+    "aarch64-darwin"
+    "x86_64-darwin"
+  ];
+
   # Helper function for generating home-manager configs
   mkHome =
     { hostname
@@ -43,12 +52,29 @@
         [ ../hosts ] ++ (inputs.nixpkgs.lib.optional isISO cd-dvd);
     };
 
-  # Supported systems
-  forAllSystems = inputs.nixpkgs.lib.genAttrs [
-    "aarch64-linux"
-    "i686-linux"
-    "x86_64-linux"
-    "aarch64-darwin"
-    "x86_64-darwin"
-  ];
+  # Helper to wrap package with NixGL (https://github.com/nix-community/nixGL)
+  # Source: https://github.com/Smona/nixpkgs/blob/f3d21833495edd036c245a0c4899e28e94c08362/applications/nixGL.nix#L4
+  wrapNixGL = { pkg, platform ? "x86_64-linux" }: (pkg.overrideAttrs (prev: {
+    name = "nixGL-${pkg.name}";
+
+    buildCommand = ''
+      set -eo pipefail
+
+      ${inputs.nixpkgs.lib.concatStringsSep "\n" (map (outputName: ''
+        echo "Copying output ${outputName}"
+        set -x
+        cp -rs --no-preserve=mode "${pkg.${outputName}}" "''$${outputName}"
+        set +x
+      '') (prev.outputs or [ "out" ]))}
+
+      rm -rf $out/bin/*
+      shopt -s nullglob # Prevent loop from running if no files
+      for file in ${pkg.out}/bin/*; do
+        echo "#!${inputs.nixpkgs.legacyPackages.${platform}.bash}/bin/bash" > "$out/bin/$(basename $file)"
+        echo "exec -a \"\$0\" ${inputs.nixgl.packages.${platform}.nixGLIntel}/bin/nixGLIntel $file \"\$@\"" >> "$out/bin/$(basename $file)"
+        chmod +x "$out/bin/$(basename $file)"
+      done
+      shopt -u nullglob # Revert nullglob back to its normal default state
+    '';
+  }));
 }
