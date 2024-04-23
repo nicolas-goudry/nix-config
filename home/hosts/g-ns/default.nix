441037445b00a91815a5792f2de3cddaccdc8ba3
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-23 15:53:20 +0200

    feat(home/g-ns): use wrapped alacritty

--- home/hosts/g-ns/default.nix
+++ home/hosts/g-ns/default.nix
@@ -1,11 +1,15 @@
-{ hostname, lib, pkgs, username, ... }:
+{ hostname, lib, outputs, pkgs, username, ... }:
 
 {
   home.shellAliases = {
     switch-home = "cd ~/nixstrap && home-manager --extra-experimental-features flakes --extra-experimental-features nix-command switch -b backup --flake .#${username}@${hostname} && cd -";
   };
 
-  programs.git = {
-    userEmail = lib.mkForce "nicolas.goudry-ext@numspot.com";
+  programs = {
+    alacritty.package = lib.mkForce (outputs.libx.wrapNixGL { pkg = pkgs.unstable.alacritty; });
+
+    git = {
+      userEmail = lib.mkForce "nicolas.goudry-ext@numspot.com";
+    };
   };
 }
