0ad78063210ca530c59035aa1c46f1e3edb58053
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-04-24 15:00:56 +0200

    refactor(hosts/g-aero): replace bcachefs by btrfs

--- hosts/g-aero/disks.nix
+++ hosts/g-aero/disks.nix
@@ -13,19 +13,17 @@
           partitions = {
             ESP = {
               type = "EF00";
-              name = "esp";
-              end = "1024M";
+              end = "1G";
 
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "defaults" "umask=0077" ];
+                mountOptions = [ "defaults" ];
               };
             };
 
-            root = {
-              name = "root";
+            luks = {
               size = "100%";
 
               content = {
@@ -35,10 +33,34 @@
                 passwordFile = "/tmp/data.passwordFile";
 
                 content = {
-                  type = "filesystem";
-                  format = "bcachefs";
-                  mountpoint = "/";
-                  mountOptions = [ "defaults" ];
+                  type = "btrfs";
+
+                  # mkfs.btrfs extra arguments (https://btrfs.readthedocs.io/en/latest/mkfs.btrfs.html)
+                  # - "-f": force overwrite block devices when existing fs is detected
+                  extraArgs = [ "-f" ];
+
+                  # btrfs mount options: https://btrfs.readthedocs.io/en/latest/btrfs-man5.html#mount-options
+                  subvolumes = {
+                    root = {
+                      mountpoint = "/";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+
+                    home = {
+                      mountpoint = "/home";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+
+                    nix = {
+                      mountpoint = "/nix";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+
+                    persist = {
+                      mountpoint = "/persist";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                  };
                 };
               };
             };
