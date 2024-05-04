f4ae49c0ff9e792bf7c611d9b69534cee9681138
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-05-04 23:01:15 +0200

    feat(home/nicolas): enable and configure gitkraken

--- home/users/nicolas/default.nix
+++ home/users/nicolas/default.nix
@@ -1,10 +1,13 @@
-{ hostname, pkgs, ... }:
+{ config, hostname, lib, outputs, pkgs, ... }:
 
 {
+  imports = [
+    outputs.homeManagerModules.gitkraken
+  ];
+
   # Install user packages depending on host
   home.packages = with pkgs; [
     bruno
-    gitkraken
     kubectl
     kubernetes-helm
     k9s
@@ -22,13 +25,61 @@
   ];
 
   # User-specific git configuration
-  programs.git = {
-    userName = "Nicolas Goudry";
-    userEmail = "goudry.nicolas@gmail.com";
+  programs = {
+    git = {
+      userName = "Nicolas Goudry";
+      userEmail = "goudry.nicolas@gmail.com";
 
-    signing = {
-      key = "EC6884FA72B9465A";
-      signByDefault = true;
+      signing = {
+        key = "EC6884FA72B9465A";
+        signByDefault = true;
+      };
+    };
+
+    gitkraken = {
+      enable = true;
+      package = pkgs.unstable.gitkraken;
+      acceptEULA = true;
+      collapsePermanentTabs = true;
+      enableCloudPatch = true;
+      logLevel = "extended";
+      skipTour = true;
+
+      commitGraph = {
+        showAuthor = true;
+      };
+
+      gpg = {
+        signingKey = config.programs.git.signing.key;
+        signCommits = config.programs.git.signing.signByDefault;
+        signTags = config.programs.git.signing.signByDefault;
+      };
+
+      notifications = {
+        enable = true;
+        marketing = false;
+      };
+
+      tools.terminal = {
+        default = "custom";
+        package = pkgs.unstable.alacritty;
+      };
+
+      ui = {
+        cli.autocomplete.tabBehavior = "navigation";
+        hideWorkspaceTab = true;
+        theme = "dark";
+
+        editor = {
+          tabSize = 2;
+          wrap = true;
+        };
+      };
+
+      user = {
+        email = config.programs.git.userEmail;
+        name = config.programs.git.userName;
+      };
     };
   };
 }
