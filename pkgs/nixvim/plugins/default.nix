9ef91041501be4e62c718ba43bde683b9f73685e
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-05-13 10:59:38 +0200

    feat(pkgs/nixvim): add support for unsupported plugins

--- pkgs/nixvim/plugins/default.nix
+++ pkgs/nixvim/plugins/default.nix
@@ -1,16 +1,27 @@
 # This file will load all plugins defined in the current directory (plugins)
 # and load their content in the 'plugins' nixvim attribute.
-# Each plugin file must have the same name as the plugin attribute name
-# expected by nixvim (ie. 'telescope.nix' for 'plugins.telescope').
+
 # Each plugin file must export a lambda, which is called with an attrset
 # containing nixpkgs library as 'lib'. Plugins may or may not use it.
-# Each plugin lambda must return an attrset with at least the 'opts' attribute,
-# this attribute is the plugin options as expected by nixvim. Additionally, a
-# 'rootOpts' can be returned along with 'opts', this attribute is extra options
-# to set to the root nixvim options. For example, this allows to set keymaps
-# for plugins that do not have an internal option to set keymaps.
 
-{ lib, ... }:
+# For plugins natively supported by nixvim, the plugin file must have the same
+# name as the plugin attribute name expected by nixvim (ie. 'telescope.nix' for
+# 'plugins.telescope'). The plugin lambda must return an attrset with at least
+# the 'opts' attribute, this attribute is the plugin options as expected by
+# nixvim.
+
+# For plugins not supported by nixvim, the plugin file can have any name. The
+# plugin lambda must return an attrset with the 'extra' attribute which is also
+# an attrset with two attributes: 'package' and 'config'. The 'package'
+# attribute is the plugin package in nixpkgs (ie. vimPlugins.<plugin>). The
+# 'config' attribute is the plugin configuration in Lua format.
+
+# Additionally, a 'rootOpts' can be returned alongside other attributes, this
+# attribute will be used to set extra options to the root nixvim options. For
+# example, this allows to set keymaps for plugins that do not have an internal
+# option to set keymaps.
+
+{ lib, pkgs, ... }:
 
 let
   definitions = lib.attrNames (
@@ -22,11 +33,15 @@ in lib.mkMerge (
   map (file:
     let
       pluginName = lib.elemAt (lib.splitString "." file) 0;
-      plugin = import ./${file} { inherit lib; };
+      plugin = import ./${file} { inherit lib pkgs; };
     in lib.mkMerge [
-      {
+      (if plugin ? opts then {
         plugins.${pluginName} = plugin.opts;
-      }
+      } else {})
+      (if plugin ? extra then {
+        extraPlugins = [ plugin.extra.package ];
+        extraConfigLua = plugin.extra.config;
+      } else {})
       (if plugin ? rootOpts then plugin.rootOpts else { })
     ]
   ) definitions
