1d53977ffd9e21f335167369963d5d950be2462b
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-05-23 18:58:30 +0200

    feat(pkgs/nixvim): add new keymaps

--- pkgs/nixvim/nixvim.nix
+++ pkgs/nixvim/nixvim.nix
@@ -37,11 +37,36 @@
       };
     };
 
-    keymaps = [
+    keymaps = let
+      forceWrite = {
+        action = "<cmd>silent! update! | redraw<cr>";
+        options.desc = "Force write";
+      };
+    in [
       {
         key = "<leader>n";
         action = "<cmd>enew<cr>";
         options.desc = "New file";
+      }
+      {
+        inherit (forceWrite) action options;
+        key = "<c-s>";
+      }
+      {
+        inherit (forceWrite) options;
+        mode = [ "i" "x" ];
+        key = "<c-s>";
+        action = "<esc>${forceWrite.action}";
+      }
+      {
+        key = "|";
+        action = "<cmd>vsplit<cr>";
+        options.desc = "Split vertically";
+      }
+      {
+        key = "\\";
+        action = "<cmd>split<cr>";
+        options.desc = "Split horizontally";
       }
     ];
 
