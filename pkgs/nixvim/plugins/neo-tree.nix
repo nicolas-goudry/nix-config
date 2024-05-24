a2b23906b14732dc1a9a7d66cf9b9a1966679f3e
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-05-24 23:44:59 +0200

    feat(pkgs/nixvim): add copy selector command to neo-tree

--- pkgs/nixvim/plugins/neo-tree.nix
+++ pkgs/nixvim/plugins/neo-tree.nix
@@ -47,6 +47,41 @@ _:
           end
         '';
 
+        # Copy various path format of currently focused item
+        copy_selector.__raw = ''
+          function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local vals = {
+              ["BASENAME"] = modify(filename, ":r"),
+              ["EXTENSION"] = modify(filename, ":e"),
+              ["FILENAME"] = filename,
+              ["PATH (CWD)"] = modify(filepath, ":."),
+              ["PATH (HOME)"] = modify(filepath, ":~"),
+              ["PATH"] = filepath,
+              ["URI"] = vim.uri_from_fname(filepath),
+            }
+
+            local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+            if vim.tbl_isempty(options) then
+              return
+            end
+            table.sort(options)
+            vim.ui.select(options, {
+              prompt = "Choose to copy to clipboard:",
+              format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+            }, function(choice)
+              local result = vals[choice]
+              if result then
+                vim.fn.setreg("+", result)
+              end
+            end)
+          end
+        '';
+
         find_file_in_dir.__raw = ''
           function(state)
             local node = state.tree:get_node()
@@ -96,6 +131,7 @@ _:
           l = "child_or_open";
           F = "find_file_in_dir";
           W = "grep_in_dir";
+          Y = "copy_selector";
         };
       };
     };
