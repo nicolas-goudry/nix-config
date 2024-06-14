80dbd78bb15b768c44955afd01f2f4ed44b184a9
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-14 18:44:29 +0200

    fix(hosts/users/root): avoid warning about multiple password options set

--- hosts/common/users/root/default.nix
+++ hosts/common/users/root/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   # Define sops source for root user password
@@ -9,7 +9,13 @@
 
   # Define user (note: use mkpasswd to create password hash)
   users.users.root = {
+    # Required for warningless ISO build since base installation cd-dvd set it to an empty string
+    # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/profiles/installation-device.nix#L40
+    initialHashedPassword = lib.mkForce null;
+
     hashedPasswordFile = config.sops.secrets.root-password.path;
+
+    # Set authorized SSH keys
     #openssh.authorizedKeys.keys = [
     #  "<key-here>"
     #];
