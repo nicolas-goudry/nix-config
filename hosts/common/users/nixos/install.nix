a7a254852e6abf514bbcea5b6c1126c6413844a4
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-06-14 15:18:08 +0200

    fix(install-script): move host RSA key to /persist
    This is required as with impermanence setup the key will be searched in /persist instead of /etc

--- hosts/common/users/nixos/install.nix
+++ hosts/common/users/nixos/install.nix
@@ -349,7 +349,7 @@
     popd > /dev/null
 
     # Move generated SSH RSA key to host filesystem
-    sudo ${pkgs.coreutils}/bin/mv /tmp/ssh_host_rsa_key /mnt/etc/ssh
+    sudo ${pkgs.coreutils}/bin/mv /tmp/ssh_host_rsa_key /mnt/persist/etc/ssh
 
     # Rsync nix-config to the new host and set the remote origin to SSH for later use
     ${pkgs.rsync}/bin/rsync -a --delete "$LOCAL_CLONE_DIR" "/mnt/home/$TARGET_USER/"
