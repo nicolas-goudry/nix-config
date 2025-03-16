675eba84a74f21a43f53bef2cdf0a6185fdec8b7
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-03-16 10:48:00 +0100

    fix(hosts/scripts): invalid usage of filterAttrs (args)

--- hosts/common/scripts/default.nix
+++ hosts/common/scripts/default.nix
@@ -5,7 +5,7 @@ let
   importDirectory = name: import (currentDir + "/${name}");
   directories = lib.filterAttrs (
     name: type: type == "directory" && name != "_template"
-  ) builtins.readDir currentDir;
+  ) (builtins.readDir currentDir);
 in
 {
   imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
