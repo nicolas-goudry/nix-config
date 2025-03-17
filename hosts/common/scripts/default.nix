3f59cb3bdaccf012a425f23ea17f80aa3815e06c
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-03-17 01:39:13 +0100

    style(hosts/scripts): format

--- hosts/common/scripts/default.nix
+++ hosts/common/scripts/default.nix
@@ -3,9 +3,9 @@
 let
   currentDir = ./.;
   importDirectory = name: import (currentDir + "/${name}");
-  directories = lib.filterAttrs (
-    name: type: type == "directory" && name != "_template"
-  ) (builtins.readDir currentDir);
+  directories = lib.filterAttrs (name: type: type == "directory" && name != "_template") (
+    builtins.readDir currentDir
+  );
 in
 {
   imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
