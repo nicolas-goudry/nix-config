f7953e90ba421b6c35481f06729339b2210e4b05
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-11-23 01:38:38 +0100

    refactor(pkgs): update packages handling
    This also removes catppuccin-gitkraken to account for new NixKraken version

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,25 +3,32 @@
 {
   # Custom packages from the 'pkgs' directory
   additions =
-    final: _prev:
-    import ../pkgs final.pkgs
+    final: prev:
+    let
+      localPkgs = prev.lib.packagesFromDirectoryRecursive {
+        inherit (prev.pkgs) callPackage;
+        directory = ../pkgs;
+      };
+    in
+    localPkgs
     // {
-      nixvim = inputs.nixvim-config.packages.${_prev.system}.default;
-      nixvim-lite = inputs.nixvim-config.packages.${_prev.system}.lite;
+      inherit (inputs.nixkraken.packages.${prev.stdenv.hostPlatform.system}) gitkraken-themes;
+      nixvim = inputs.nixvim-config.packages.${prev.stdenv.hostPlatform.system}.default;
+      nixvim-lite = inputs.nixvim-config.packages.${prev.stdenv.hostPlatform.system}.lite;
     };
 
   # Change versions, add patches, set compilation flags, etc...
   # https://wiki.nixos.org/wiki/Overlays
-  modifications = _final: _prev: {
+  modifications = final: prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
   };
 
   # Unstable nixpkgs set through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
+  unstable-packages = final: prev: {
     unstable = import inputs.nixpkgs-unstable {
-      inherit (final) system;
+      inherit (prev.stdenv.hostPlatform) system;
       config.allowUnfree = true;
     };
   };
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,9 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # Build them using 'nix build .#example'
 
-pkgs:
+{ lib, callPackage, ... }:
 
-{
-  # example = pkgs.callPackage ./example.nix { };
-  catppuccin-delta = pkgs.callPackage ./catppuccin-themes/delta.nix { };
-  catppuccin-gitkraken = pkgs.callPackage ./catppuccin-themes/gitkraken.nix { };
-  kdrive = pkgs.callPackage ./kdrive { };
-  omz-custom-plugins = pkgs.callPackage ./omz-custom-plugins { };
+lib.packagesFromDirectoryRecursive {
+  inherit callPackage;
+  directory = ./.;
 }
