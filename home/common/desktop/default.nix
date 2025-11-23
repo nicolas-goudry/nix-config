7d68c2d794c9484a1d3011b5caa0dd7277e06eb5
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-11-23 01:44:10 +0100

    feat(home+hosts): configure chromium browser

--- home/common/desktop/default.nix
+++ home/common/desktop/default.nix
@@ -37,6 +37,35 @@ in
       libreoffice-fresh # Productivity suite
     ];
 
+  # Chromium Browser
+  # NOTE: ideally, extensions are NOT declared here, but in programs.chromium.extensions in /hosts/common/desktop/default.nix
+  #       This allows for a common Chromium-based browsers configuration
+  programs.chromium = {
+    enable = true;
+    package = pkgs.unstable.chromium;
+
+    commandLineArgs = [
+      # Hide UI elements in fullscreen (X button and hint popup)
+      "--hide-fullscreen-exit-ui"
+      # Remove tabsearch button from tabstrip
+      "--remove-tabsearch-button"
+      # Disable top sites from new tab page
+      "--disable-top-sites"
+      # Force dark mode
+      "--force-dark-mode"
+      # Disable default browser check
+      "--no-default-browser-check"
+    ];
+
+    # Spell check dictionaries to install
+    # WARN: unless configured by policies, dictionaries are not used by default
+    #       See programs.chromium.extraOpts in /hosts/common/desktop/default.nix for an example
+    dictionaries = [
+      pkgs.hunspellDictsChromium.fr_FR
+      pkgs.hunspellDictsChromium.en_US
+    ];
+  };
+
   services.earth-view = {
     enable = isLinux;
     interval = "4h";
--- hosts/common/desktop/default.nix
+++ hosts/common/desktop/default.nix
@@ -20,6 +20,7 @@
 let
   # Precompute predicates
   hasNvidia = builtins.elem "nvidia" config.services.xserver.videoDrivers;
+  isGnome = desktop == "gnome";
 in
 {
   imports = lib.optional (builtins.pathExists (./. + "/${desktop}")) ./${desktop};
@@ -63,7 +64,6 @@ in
     with pkgs;
     [
       ghostty
-      brave
     ]
     # Add WPA GUI if wireless networks are enabled
     ++ lib.optional config.networking.wireless.enable wpa_supplicant_gui;
@@ -173,6 +173,97 @@ in
         sync.enable = true;
       };
     };
+  };
+
+  # Configure common policies for Chromium-based browsers (ie. Chromium, Google Chrome and Brave)
+  # Can be disabled (or further tweaked) per-host, maybe using lib.mkForce
+  programs.chromium = {
+    # WARN: this does not install Chromium!!!
+    enable = lib.mkDefault true;
+    # Default home is the Bonjourr extension page
+    homepageLocation = lib.mkDefault "chrome-extension://dlnejlppicbjfcfcedcflplfjajinajd/index.html";
+    # Default search provider
+    defaultSearchProviderEnabled = true;
+    defaultSearchProviderSearchURL = "https://duckduckgo.com/?q={searchTerms}";
+    defaultSearchProviderSuggestURL = "https://duckduckgo.com/ac/?q={searchTerms}&type=list";
+
+    # Reference: https://www.chromium.org/administrators/configuring-other-preferences/
+    initialPrefs = lib.mkDefault {
+      browser.show_home_button = false;
+      bookmark_bar.show_on_all_tabs = true;
+      sync_promo.show_on_first_run_allowed = false;
+      # Automatically restore previous session on startup
+      session.restore_on_startup = 1;
+
+      # Set first run tab to new tab page (extensions are not installed yet)
+      first_run_tabs = [
+        "chrome://new-tab-page"
+      ];
+    };
+
+    # Reference: https://chromeenterprise.google/intl/en_us/policies/
+    extraOpts = {
+      # Disable built-in password manager (we use Bitwarden)
+      PasswordManagerEnabled = false;
+      PasswordManagerPasskeysEnabled = false;
+      # Always select system default printer in print preview
+      PrintPreviewUseSystemDefaultPrinter = true;
+      # Disable subtitles translation (data is sent to Google)
+      LiveTranslationEnabled = false;
+      # Always ask for geolocation
+      DefaultGeolocationSetting = 3;
+      # Always ask for notifications
+      DefaultNotificationsSetting = 3;
+      # Disable popups on all sites
+      DefaultPopupsSetting = 2;
+      # Disable credit card suggestions
+      AutofillCreditCardEnabled = false;
+      # Remove Chrome Labs icon from toolbar
+      BrowserLabsEnabled = false;
+      # Disable Chrome promotion pages
+      PromotionsEnabled = false;
+      # Spell check languages (must be installed)
+      "SpellcheckLanguage" = [
+        "en-US"
+        "fr"
+      ];
+
+      # Extensions configuration
+      # Reference: https://www.chromium.org/administrators/policy-list-3/extension-settings-full/
+      ExtensionSettings = {
+        # Force extensions installation from Nix configuration
+        "*" = {
+          installation_mode = "blocked";
+          blocked_install_message = "Extensions must be configured in NixOS/Home Manager configuration";
+        };
+
+        # Bitwarden Password Manager
+        nngceckbapebfimnlniiiahkandclblb = {
+          toolbar_pin = "default_pinned";
+        };
+
+        # Ghostery
+        mlomiejdfkolichcflejclcbmpeaniij = {
+          toolbar_pin = "default_pinned";
+        };
+      };
+    };
+
+    # Basic set of extensions
+    extensions = [
+      # Bitwarden Password Manager
+      "nngceckbapebfimnlniiiahkandclblb"
+      # Bonjourr Minimalist Startpage
+      "dlnejlppicbjfcfcedcflplfjajinajd"
+      # Ghostery
+      "mlomiejdfkolichcflejclcbmpeaniij"
+      # GNOME Shell Integration
+      (lib.mkIf isGnome "gphhapmejobijbbhgpjhcjognlahblep")
+      # Accept all cookies
+      "ofpnikijgfhlmmjlpkfaifhhdonchhoi"
+      # JSON Viewer Pro
+      "eifflpmocdbdmepbjaopkkhbfmdgijcc"
+    ];
   };
 
   services = {
